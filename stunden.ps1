941c39e3e3c030635794afd7a4f96b5bfcb0f614
Porfirev <Daniil.Porfirev@sulzer.de>
2025-07-31 15:58:45 +0200

    Minor grammatical changes

--- stunden.ps1
+++ stunden.ps1
@@ -1,4 +1,4 @@
-$pauseMinuten = Read-Host "Bitte gib deine gewuenschte Pause in Minuten ein"
+$pauseMinuten = Read-Host "Bitte gewuenschte Pause in Minuten eingeben"
 $sollArbeitszeit = New-TimeSpan -Hours 7 -Minutes 54
 try {
     $ankunftszeit = [datetime]::ParseExact((Read-Host "Bitte Ankunftszeit eingeben (HH:mm, z.B. 07:30)"),"HH:mm",$null)
@@ -8,4 +8,4 @@ $feierabendSoll = $ankunftszeit.AddMinutes($pauseMinuten).Add($sollArbeitszeit)
 $feierabendWunsch = $ankunftszeit.AddMinutes($pauseMinuten).Add($arbeitszeitWunsch)
 Write-Host "`nFeierabend (Sollzeit):    $($feierabendSoll.ToString('HH:mm'))"
 Write-Host  "Feierabend (Wunschzeit): $($feierabendWunsch.ToString('HH:mm'))"
-Read-Host "`nZum Schliessen Enter druecken..."
+Read-Host "`nZum Schliessen Enter druecken"
