a0d88d130fd8c842594463aa8faba82769a5e80b
Porfirev <Daniil.Porfirev@sulzer.de>
2025-07-31 12:56:45 +0200

    Add initial Powershell script

---
+++ stunden.ps1
@@ -0,0 +1,11 @@
+$pauseMinuten = 30
+$sollArbeitszeit = New-TimeSpan -Hours 7 -Minutes 54
+try {
+    $ankunftszeit = [datetime]::ParseExact((Read-Host "Bitte Ankunftszeit eingeben (HH:mm, z.B. 07:30)"),"HH:mm",$null)
+    $arbeitszeitWunsch = [TimeSpan]::Parse((Read-Host "Bitte gewuenschte Arbeitszeit eingeben (HH:mm, z.B. 08:30)"))
+} catch { Write-Host "Ungueltige Eingabe!" ; exit }
+$feierabendSoll = $ankunftszeit.AddMinutes($pauseMinuten).Add($sollArbeitszeit)
+$feierabendWunsch = $ankunftszeit.AddMinutes($pauseMinuten).Add($arbeitszeitWunsch)
+Write-Host "`nFeierabend (Sollzeit):    $($feierabendSoll.ToString('HH:mm'))"
+Write-Host  "Feierabend (Wunschzeit): $($feierabendWunsch.ToString('HH:mm'))"
+Read-Host "`nZum Schliessen Enter druecken..."
