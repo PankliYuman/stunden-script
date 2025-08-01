74e4583be639baa01d2766bd083fc92d45d83294
Porfirev <Daniil.Porfirev@sulzer.de>
2025-08-01 15:15:47 +0200

    Standartwerte fuer Pause und Wunscharbeitszeit hinzugefuegt

--- stunden.ps1
+++ stunden.ps1
@@ -1,11 +1,29 @@
-$pauseMinuten = Read-Host "Bitte gewuenschte Pause in Minuten eingeben"
+$pauseMinutenEingabe = Read-Host "Bitte gewuenschte Pause in Minuten eingeben (leer = 30)"
+if ([string]::IsNullOrWhiteSpace($pauseMinutenEingabe)) {
+    $pauseMinuten = 30
+} else {
+    $pauseMinuten = [int]$pauseMinutenEingabe
+}
+
 $sollArbeitszeit = New-TimeSpan -Hours 7 -Minutes 54
+
 try {
     $ankunftszeit = [datetime]::ParseExact((Read-Host "Bitte Ankunftszeit eingeben (HH:mm, z.B. 07:30)"),"HH:mm",$null)
-    $arbeitszeitWunsch = [TimeSpan]::Parse((Read-Host "Bitte gewuenschte Arbeitszeit eingeben (HH:mm, z.B. 08:30)"))
-} catch { Write-Host "Ungueltige Eingabe!" ; exit }
+    
+    $arbeitszeitEingabe = Read-Host "Bitte gewuenschte Arbeitszeit eingeben (HH:mm, leer = 07:54)"
+    if ([string]::IsNullOrWhiteSpace($arbeitszeitEingabe)) {
+        $arbeitszeitWunsch = $sollArbeitszeit
+    } else {
+        $arbeitszeitWunsch = [TimeSpan]::Parse($arbeitszeitEingabe)
+    }
+} catch { 
+    Write-Host "Ungueltige Eingabe!" 
+    exit 
+}
+
 $feierabendSoll = $ankunftszeit.AddMinutes($pauseMinuten).Add($sollArbeitszeit)
 $feierabendWunsch = $ankunftszeit.AddMinutes($pauseMinuten).Add($arbeitszeitWunsch)
+
 Write-Host "`nFeierabend (Sollzeit):    $($feierabendSoll.ToString('HH:mm'))"
-Write-Host  "Feierabend (Wunschzeit): $($feierabendWunsch.ToString('HH:mm'))"
+Write-Host "Feierabend (Wunschzeit): $($feierabendWunsch.ToString('HH:mm'))"
 Read-Host "`nZum Schliessen Enter druecken"
